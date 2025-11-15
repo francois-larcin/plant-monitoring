be44f591d5e85bc215b59cc22fbbf3600e7c4d06
francois-larcin <francoislarcin@hotmail.com>
2025-11-15 21:11:02 +0100

    set mocked datas, interface and enums

---
+++ src/models/enums/soil-moisture-status-enum.ts
@@ -0,0 +1,5 @@
+export enum SoilMoistureStatus {
+  Low = 'low',
+  Medium = 'medium',
+  High = 'high',
+}
---
+++ src/models/interfaces/plant-measurement-interface.ts
@@ -0,0 +1,41 @@
+import { LightStatus } from "../enums/light-status-enum";
+import { SoilMoistureStatus } from "../enums/soil-moisture-status-enum";
+
+
+export interface PlantMeasurement {
+  plantId: string;
+  timestamp: string; // ISO 8601 format
+
+  soilMoisture: {
+    value: number;
+    unit: string;
+    status: SoilMoistureStatus;
+  };
+
+  temperature: {
+    value: number;
+    unit: string;
+  };
+
+  humidity: {
+    value: number;
+    unit: string;
+  };
+
+  light: {
+    value: number;
+    unit: string;
+    status: LightStatus;
+  };
+
+  waterPump: {
+    isActive: boolean;
+    lastRun: string; // ISO 8601 format
+    autoMode: boolean;
+  };
+
+  battery: {
+    value: number;
+    unit: string;
+  };
+}
